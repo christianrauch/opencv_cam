234767ae26cfef16a1ee9cf6ad93983145aa96cd
Christian Rauch <Rauch.Christian@gmx.de>
2020-08-05 00:47:09 +0100

    rename deprecated parameters

--- launch/cam_vga.launch.py
+++ launch/cam_vga.launch.py
@@ -3,14 +3,14 @@ from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
-    composable_node = ComposableNode(node_name='camera',
-    package='opencv_cam', node_plugin='CameraNode',
+    composable_node = ComposableNode(name='camera',
+    package='opencv_cam', plugin='CameraNode',
     parameters=[{"width": 640, "height": 480}])
     container = ComposableNodeContainer(
-            node_name='camera_container',
-            node_namespace='camera',
+            name='camera_container',
+            namespace='camera',
             package='rclcpp_components',
-            node_executable='component_container',
+            executable='component_container',
             composable_node_descriptions=[composable_node],
             output='screen',
     )
